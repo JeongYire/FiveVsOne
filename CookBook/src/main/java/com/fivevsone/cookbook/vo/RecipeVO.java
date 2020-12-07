5519d3c0e60b0988cef9c325dbca75e8140ebcf0
정이레 <@DESKTOP-J33RI4U>
2020-12-07 16:19:56 +0900

    wjddlfp

--- CookBook/src/main/java/com/fivevsone/cookbook/vo/RecipeVO.java
+++ CookBook/src/main/java/com/fivevsone/cookbook/vo/RecipeVO.java
@@ -3,19 +3,14 @@ package com.fivevsone.cookbook.vo;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlTransient;
+
 
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-@XmlAccessorType(XmlAccessType.FIELD)
 public class RecipeVO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	@XmlAttribute
+	
 	private int recipeCode;
 	private int natCode;
 	private String natName;
@@ -44,11 +39,11 @@ public class RecipeVO implements Serializable {
 	private int recipeState;
 
 	
-	@XmlTransient
+
 	private MultipartFile file;
-	@XmlTransient
+	
 	private String searchCondition;
-	@XmlTransient
+	
 	private String searchKeyword;
 	
 	public RecipeVO() {
@@ -180,21 +175,21 @@ public class RecipeVO implements Serializable {
 	public void setRecipeState(int recipeState) {
 		this.recipeState = recipeState;
 	}
-	@JsonIgnore
+	
 	public MultipartFile getFile() {
 		return file;
 	}
 	public void setFile(MultipartFile file) {
 		this.file = file;
 	}
-	@JsonIgnore
+	
 	public String getSearchCondition() {
 		return searchCondition;
 	}
 	public void setSearchCondition(String searchCondition) {
 		this.searchCondition = searchCondition;
 	}
-	@JsonIgnore
+	
 	public String getSearchKeyword() {
 		return searchKeyword;
 	}
