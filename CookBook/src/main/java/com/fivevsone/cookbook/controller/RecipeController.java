2e6bc013e8d2f50ded9cf1f8c710ea1d345d76fb
keh8093 <keh8093@naver.com>
2020-12-07 16:37:58 +0900

    정이레가출가ㅏㅋ

--- CookBook/src/main/java/com/fivevsone/cookbook/controller/RecipeController.java
+++ CookBook/src/main/java/com/fivevsone/cookbook/controller/RecipeController.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Controller;
 import com.fivevsone.cookbook.dao.MemberService;
 import com.fivevsone.cookbook.dao.RecipeService;
 
-
-public class BoardController {
+@Controller
+public class RecipeController {
 	// BoardController 는 정이레가 임시로 붙인 이름입니다 컨트롤러를 담당하시는분이 이름을 정하세요
 	
-	
+	@Autowired
 	MemberService memberService;
 	RecipeService recipeService;
 
