a1a52eb3bac79441159fce1d0c30e04469d4e3fe
정이레 <@DESKTOP-J33RI4U>
2020-12-07 16:50:48 +0900

    정이레테스트 추가

--- CookBook/src/main/java/com/fivevsone/cookbook/test/JeongYireTest.java
+++ CookBook/src/main/java/com/fivevsone/cookbook/test/JeongYireTest.java
@@ -29,19 +29,10 @@ public class JeongYireTest {
 		
 		   MemberVO vo = new MemberVO();
 		   
-		  vo.setMemId("yire2");
-		   vo.setMemPwd("yire1234");
-		   vo.setMemName("정이레");
-		   vo.setMemNickName("정이레몬");
-		   vo.setMemEmail("wotjd0611@gmail.com");
-		   vo.setMemGender("남자");
-		   vo.setBookMark("14,72,66,41,45,6");
-		   vo.setMemIntroduce("정이레입니다.");
-		   vo.setSitCode("1,2,3,4,11");
-		   vo.setNatCode("11,44,23,55");
+		 
 		      System.out.println(vo);
 		   
-		 service.insertMember(vo);
+		
 		   
 		   System.out.println(service.selectAllMember());
 		   
